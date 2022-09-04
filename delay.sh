e8a3de8d0117a85d0af59202ec7c45f74a115dfd
invisan <da_invisible@freenet.de>
2022-09-04 22:48:24 +0200

    Create delay.sh

---
+++ delay.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+server=$1
+application=$2
+key=$3
+
+###DONT CHANGE ANYTHING BELOW THIS IF YOU DONT KNOW WHAT YOU ARE DOING!!!#
+#Get the Variable so we can split it
+path=$4
+file=$5
+time=${5%-*}
+time=${time%-*}
+numbers=${time%?}
+format=${time: -1}
+fpath="$path/$file"
+jdate=$(date +%s)
+cdate=$(stat -c "%W" $fpath)
+
+echo $time
+
+
+if [ $format -eq "m" ]
+then
+        echo Minutes
+        numbers=$(expr $numbers \* 60)
+fi
+
+ndate=$(expr $cdate + $numbers)
+
+dif=$(expr $ndate - $jdate)
+
+#echo Format $format Time $time numbers $numbers Ndate $ndate jdate $jdate cdate $cdate Differenz $dif Server $server App $application Key $key
+
+sleep $dif
+
+ffmpeg="ffmpeg -re -i $fpath -acodec copy -vcodec copy -f flv rtmp://$server/$application/$key"
+
+echo $ffmpeg
+
+eval $ffmpeg
