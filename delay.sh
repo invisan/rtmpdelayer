574c142ad9c6f69be7bd1ac3758aa2ac63593bf7
invisan <da_invisible@freenet.de>
2023-02-19 14:57:28 +0100

    Update delay.sh

--- delay.sh
+++ delay.sh
@@ -1,36 +1,44 @@
 #!/bin/bash
-server=$1
-application=$2
-extserver=$3
-discord=$4
-mention1=$5
-mention2=$6
+config=$1
+
+#Get Settings from Conf File
+server=$(sed '1q;d' $config | cut -d '=' -f 2 | cut -d '"' -f 2)
+extserver=$(sed '2q;d' $config | cut -d '=' -f 2 | cut -d '"' -f 2)
+application=$(sed '3q;d' $config | cut -d '=' -f 2 | cut -d '"' -f 2)
+discord=$(sed '4q;d' $config | cut -d '=' -f 2 | cut -d '"' -f 2)
+mention1=$(sed '5q;d' $config | cut -d '=' -f 2 | cut -d '"' -f 2)
+mention2=$(sed '6q;d' $config | cut -d '=' -f 2 | cut -d '"' -f 2)
 
 
 
-#Get the Variable so we can split it
-path=$7
-file=$8
-time=${8%-*}
+path=$2
+file=$3
+time=${3%-*}
 key=${time##*-}
 time=${time%-*}
 numbers=${time%?}
 format=${time: -1}
 fpath="$path/$file"
-jdate=$(date +%s) 
+jdate=$(date +%s)
 cdate=$(stat -c "%W" $fpath)
+
+if [ "$cdate" = "0" ]
+then
+        cdate=$jdate
+fi
+
 
 if [ "$format" = "m" ]
 then
-	echo Minutes
-	numbers=$(expr $numbers \* 60)
+        echo Minutes
+        numbers=$(expr $numbers \* 60)
 fi
 
 ndate=$(expr $cdate + $numbers)
 
 dif=$(expr $ndate - $jdate)
 
-echo Pfad $path File $file Format $format Time $time numbers $numbers Ndate $ndate jdate $jdate cdate $cdate Differenz $dif Server $server App $application Key $key > ./log.txt
+echo Pfad = $path File = $file Format = $format Time = $time numbers = $numbers Ndate = $ndate jdate = $jdate cdate = $cdate Differenz = $dif Server = $server App = $application Key = $key DC Webhook = $discord > ./log.txt
 
 msg="$mention1 Delay Script gestartet. Der delayte Stream geht in $dif Sekunden live."
 
@@ -42,10 +50,10 @@ msg2=\"$msg2\"
 
 if ! [ "$discord" = "1" ]
 then
-	curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg}" $discord
+        curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg}" $discord
 fi
 
-sleep $dif 
+sleep $dif
 
 if ! [ "$discord" = "1" ]
 then
